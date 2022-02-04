fb536b07bd943ed562c85d6d23df798e6c0eb1df
g4rcez <allan.f.garcez@gmail.com>
2022-02-04 18:50:21 -0300

    fix: theme provider

--- src/root.tsx
+++ src/root.tsx
@@ -16,29 +16,35 @@ export const loader: LoaderFunction = async (ctx) => {
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: css }];
 
-export default function App() {
-  const loaderData = useLoaderData();
+const Wrap: React.VFC<{ items: any[] }> = (props) => {
   const [theme] = useTheme();
   return (
-    <ThemeProvider initialTheme={theme}>
-      <html lang="pt-BR" className={theme === Themes.Dark ? "dark" : ""}>
-        <head>
-          <meta charSet="utf-8" />
-          <title>g4rcez Blog</title>
-          <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover" />
-          <meta name="theme-color" content={ConfigJson.colors.main.default} />
-          <meta name="color-scheme" content={theme === Themes.Dark ? "dark light" : "light dark"} />
-          <Meta />
-          <Links />
-        </head>
-        <body className="w-full h-full">
-          <Navbar items={loaderData.menuItems} />
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-          {process.env.NODE_ENV === "development" && <LiveReload />}
-        </body>
-      </html>
+    <html lang="pt-BR" className={theme === Themes.Dark ? "dark" : ""}>
+      <head>
+        <meta charSet="utf-8" />
+        <title>g4rcez Blog</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover" />
+        <meta name="theme-color" content={ConfigJson.colors.main.default} />
+        <meta name="color-scheme" content={theme === Themes.Dark ? "dark light" : "light dark"} />
+        <Meta />
+        <Links />
+      </head>
+      <body className="w-full h-full">
+        <Navbar items={props.items} />
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+};
+
+export default function App() {
+  const loaderData = useLoaderData();
+  return (
+    <ThemeProvider initialTheme={loaderData.theme}>
+      <Wrap items={loaderData.menuItems} />
     </ThemeProvider>
   );
 }
