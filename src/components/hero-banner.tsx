362d7cef1e994ba58f831e1f4d8b7b691faba9ea
g4rcez <allan.f.garcez@gmail.com>
2025-01-20 14:49:26 -0300

    feat: navbar container

--- src/components/hero-banner.tsx
+++ src/components/hero-banner.tsx
@@ -46,7 +46,7 @@ export function Header() {
     return (
         <header
             className={clsx(
-                "sticky top-0 z-50 flex flex-none flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8 dark:shadow-none",
+                "container sticky top-0 z-50 mx-auto flex flex-none flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8 dark:shadow-none",
                 isScrolled
                     ? "dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75"
                     : "dark:bg-transparent",
